fbb8ad38007fc260545ceeb658d103c69a37651e
Nashit Humam <85383203+s-nashit@users.noreply.github.com>
2024-01-18 15:53:22 +0530

    Add files via upload

---
+++ http.js
@@ -0,0 +1,8 @@
+http = require('http')
+
+server = http.createServer((req,res) =>{
+    res.writeHead(200, {'Content-Type':'text/plain'})
+    res.end('<h1>hello world</h1>. uyfr, bgfdd, nashit')
+})
+
+server.listen(3000,()=>{console.log('server up')})
