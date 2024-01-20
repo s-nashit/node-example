3461f4cfdacc4dbdb13c568e532462ffadb55cbe
Nashit Humam <85383203+s-nashit@users.noreply.github.com>
2024-01-20 13:31:38 +0530

    Update http.js

--- http.js
+++ http.js
@@ -2,9 +2,11 @@
 
 http = require('http')
 
+// create a server
 server = http.createServer((req,res) =>{
     res.writeHead(200, {'Content-Type':'text/plain'})
     res.end('<h1>hello world</h1>. uyfr, bgfdd, nashit')
 })
 
+//server listening
 server.listen(3000,()=>{console.log('server up')})
