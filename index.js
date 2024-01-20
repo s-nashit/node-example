288d340391f38c859b0aa2301f430d04437ca780
Nashit Humam <85383203+s-nashit@users.noreply.github.com>
2024-01-20 13:32:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+
+// importing a local module
 mul = require('./prod.js')
 
+// calling the module
 a = mul(5,8)
 console.log(a)
 console.log('my first node app')
 
+// module
 b = mul(5,99)
 console.log(b)
 
